6e6afbba00c80bac0b9450928977d71c287a5061
obayhan <mozgurbayhan@gmail.com>
2014-08-15 23:15:39 +0300

    most done

---
+++ PresentationMakerBackEnd.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-  
+__author__ = 'ozgur'
+__creation_date__ = '13.08.2014' '00:45'
+from engine import MovieInfoFetcher, ImageProcessor
+
+
+class PresentationMakerBackEnd():
+    # from PresentationMakerBackEnd import PresentationMakerBackEnd as PMB
+    # QtCore.QObject.connect(self.btn_CreateCover, QtCore.SIGNAL(_fromUtf8("clicked()")), self.create_cover)
+    # QtCore.QObject.connect(self.btn_CreateCaps, QtCore.SIGNAL(_fromUtf8("clicked()")), self.create_caps)
+    # QtCore.QObject.connect(self.btn_SelectMovie, QtCore.SIGNAL(_fromUtf8("clicked()")), self.select_movie)
+    # QtCore.QObject.connect(self.actionFilm, QtCore.SIGNAL(_fromUtf8("activated()")), self.create_presentation_movie)
+    # QtCore.QObject.connect(self.actionMuzik, QtCore.SIGNAL(_fromUtf8("activated()")), self.create_presentation_audio)
+    # QtCore.QObject.connect(self.actionOyun, QtCore.SIGNAL(_fromUtf8("activated()")), self.create_presentation_game)
+    # QtCore.QObject.connect(self.btn_MovieInfo, QtCore.SIGNAL(_fromUtf8("clicked()")), self.get_movie_info)
+
+    def create_cover(self):
+        ip = ImageProcessor()
+        self.txt_Cover.setText(ip.prepare_image(self.txt_CoverLink.text(), "cover"))
+
+    def create_caps(self):
+        ip = ImageProcessor()
+        if self.txt_CapLink1.text() != "":
+            self.txt_Cap1.setText(ip.prepare_image(self.txt_CapLink1.text(), "cover"))
+        if self.txt_CapLink2.text() != "":
+            self.txt_Cap2.setText(ip.prepare_image(self.txt_CapLink2.text(), "cover"))
+        if self.txt_CapLink3.text() != "":
+            self.txt_Cap3.setText(ip.prepare_image(self.txt_CapLink3.text(), "cover"))
+        if self.txt_CapLink4.text() != "":
+            self.txt_Cap4.setText(ip.prepare_image(self.txt_CapLink4.text(), "cover"))
+        if self.txt_CapLink5.text() != "":
+            self.txt_Cap5.setText(ip.prepare_image(self.txt_CapLink5.text(), "cover"))
+
+    def select_movie(self):
+        pass
+
+    def create_presentation_movie(self):
+        output = ""
+        output += u"[align=center][color=#FF0000][size=x-large][b]Film Başlık[/b][/size][/color]\n"
+        output += u"[IMG]" + self.txt_Cover.text() + u"[/IMG]\n"
+        output += u"[img]http://hddiyari.net/sablonlar/Filmdetaylariii.png[/img]\n"
+        output += u"[size=small][b][color=#FF0000]Yönetmen :[/color] " + self.txt_Director.text() + u"\n"
+        output += u"[color=#FF0000]Yazar: [/color] " + self.txt_Author.text() + u"\n"
+        output += u"[color=#FF0000]Oyuncular : [/color] " + self.txt_Actors.toPlainText() + u"\n"
+        output += u"[color=#FF0000]Tür : [/color] " + self.txt_Genre.text() + u" [/b][/size]\n"
+        output += u"[img]http://hddiyari.net/sablonlar/Filmkonusu.png[/img]\n"
+        output += u"[size=small][b]" + self.txt_Description.toPlainText() + u"[/b][/size]\n"
+        output += u"[img]http://hddiyari.net/sablonlar/Fragman.png[/img]\n"
+        output += u"[youtube]" + self.txt_Youtube.text() + u"[/youtube]\n"
+        output += u"[img]http://hddiyari.net/sablonlar/EkranGoruntusu.png[/img]\n"
+        output += u"[IMG]" + self.txt_CapLink1.text() + u"[/IMG]\n"
+        output += u"[IMG]" + self.txt_CapLink2.text() + u"[/IMG]\n"
+        output += u"[IMG]" + self.txt_CapLink3.text() + u"[/IMG]\n"
+        output += u"[IMG]" + self.txt_CapLink4.text() + u"[/IMG]\n"
+        output += u"[IMG]" + self.txt_CapLink5.text() + u"[/IMG]\n"
+        output += u"[IMG]http://hddiyari.net/sablonlar/seedediniz.png[/IMG]\n"
+        output += u"[img]http://hddiyari.net/sablonlar/infobilgi.png[/img]\n"
+        output += u"[code]+" + self.txt_Info.toPlainText() + u"+[/code][/align]\n"
+
+        self.txt_Output.setPlainText(output)
+        self.tabWidget.setCurrentIndex(3)
+
+    def create_presentation_audio(self):
+        pass
+
+    def create_presentation_game(self):
+        pass
+
+    def get_movie_info(self):
+        mif = MovieInfoFetcher()
+        if self.txt_InfoUrl.text() != "":
+            url = unicode(self.txt_InfoUrl.text())
+            parsed = mif.parse_movie_divxplanet(url) or {}
+            self.txt_Head.setText(parsed["title"])
+            self.txt_Director.setText(parsed["directors"])
+            self.txt_Author.setText(parsed["authors"])
+            self.txt_Genre.setText(parsed["genre"])
+            self.txt_Actors.setText(parsed["actors"])
+            self.txt_Description.setText(parsed["description"])
--- engine.py
+++ engine.py
@@ -1,3 +1,155 @@
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
+import os
+import urllib2
+import urllib
+from mechanize import Browser
+from bs4 import BeautifulSoup
+import re
+from PIL import Image
+import pyimgur
+
+
 __author__ = 'ozgur'
 __creation_date__ = '11.08.2014' '23:15'
+CLIENT_ID = "48fa40a51f1c795"
+HDR = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+
+
+class MovieInfoFetcher():
+    def __init__(self):
+        pass
+
+    def getunicode(self, soup):
+        body = ''
+        if isinstance(soup, unicode):
+            soup = soup.replace('&#39;', "'")
+            soup = soup.replace('&quot;', '"')
+            soup = soup.replace('&nbsp;', ' ')
+            body += soup
+        else:
+            if not soup.contents:
+                return ''
+            con_list = soup.contents
+            for con in con_list:
+                body = body + self.getunicode(con)
+        return body
+
+    @staticmethod
+    def parse_movie_divxplanet(link):
+        directors = ""
+        authors = ""
+        actors = ""
+        genre = ""
+        req = urllib2.Request(link, headers=HDR)
+        page = urllib2.urlopen(req)
+        soup = BeautifulSoup(page.read())
+
+        temp_list = soup.find_all('div', itemprop='director')
+        for item in temp_list:
+            directors += item.a.span.text + ", "
+
+        temp_list = soup.find_all('div', itemprop='author')
+        for item in temp_list:
+            authors += item.a.span.text + ", "
+
+        title = soup.find('span', itemprop='alternativeHeadline').text or ""
+
+        temp_list = soup.find_all('div', itemprop='actor')
+        for item in temp_list:
+            actors += item.a.span.text + ", "
+
+        temp_list = soup.find_all('span', itemprop='genre')
+        for item in temp_list:
+            genre += item.text + ", "
+        description = soup.find('span', itemprop='description').text or ""
+
+        retval = {
+            'directors': directors,
+            'authors': authors,
+            'title': title,
+            'actors': actors,
+            'genre': genre,
+            'description': description,
+        }
+        return retval
+
+
+    def parse_movie_imdb(self, link):
+        br = Browser()
+        br.open(link)
+
+        link = br.find_link(url_regex=re.compile(r'/title/tt.*'))
+        res = br.follow_link(link)
+
+        soup = BeautifulSoup(res.read())
+
+        movie_title = self.getunicode(soup.find('title'))
+        rate = soup.find('span', itemprop='ratingValue')
+        rating = self.getunicode(rate)
+
+        actors = []
+        actors_soup = soup.findAll('a', itemprop='actors')
+        for i in range(len(actors_soup)):
+            actors.append(self.getunicode(actors_soup[i]))
+
+        des = soup.find('meta', {'name': 'description'})['content']
+
+        genre = []
+        infobar = soup.find('div', {'class': 'infobar'})
+        r = infobar.find('', {'title': True})['title']
+        genrelist = infobar.findAll('a', {'href': True})
+
+        for i in range(len(genrelist) - 1):
+            genre.append(self.getunicode(genrelist[i]))
+        release_date = self.getunicode(genrelist[-1])
+
+        print movie_title, rating + '/10.0'
+        print 'Relase Date:', release_date
+        print 'Rated', r
+        print ''
+        print 'Genre:',
+        print ', '.join(genre)
+        print '\nActors:',
+        print ', '.join(actors)
+        print '\nDescription:'
+        print des
+
+
+class ImageProcessor():
+    def __init__(self):
+        pass
+
+    def _download_image(self, link, path):
+        print(link + " >> " + path)
+        testfile = urllib.URLopener(HDR)
+        testfile.retrieve(link, path)
+        testfile.close()
+
+    def _resize_image(self, path):
+        size = 500, 500
+        im = Image.open(path)
+        im.thumbnail(size, Image.ANTIALIAS)
+        im.save(path)
+
+    def _upload_image(self, path):
+        im = pyimgur.Imgur(CLIENT_ID)
+        uploaded_image = im.upload_image(path, title="HDDiyari")
+        return uploaded_image.link
+
+
+    def prepare_image(self, link, name):
+        retval = ""
+        link = str(link)
+        if not os.path.exists("temp"):
+            os.makedirs("temp")
+        if link != "":
+            path = os.path.join("temp", name) + ".jpg"
+            self._download_image(link, path)
+            self._resize_image(path)
+            retval = self._upload_image(path)
+        return retval
