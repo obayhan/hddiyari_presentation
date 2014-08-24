f368cfc0320dba512f2dc0348cfec3089cb8888a
ozgur <mozgurbayhan@gmail.com>
2014-08-24 13:44:24 +0300

    setup maker added

---
+++ builder.bat
@@ -0,0 +1,3 @@
+del C:\Users\ozgur\Desktop\myworks\workspaces\pyCharmProjects\hddiyari_presentation\build
+cd C:\Users\ozgur\Desktop\myworks\workspaces\pyCharmProjects\hddiyari_presentation
+C:\Python27\Scripts\cxfreeze PresentationMaker.py --target-dir build --base-name=Win32GUI --icon=resources\animals-bear.ico
